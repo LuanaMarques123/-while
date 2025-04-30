3e4da201157635b853dda43346cd0e545a7a6353
J.U.!.C.£ <marquessilvaluana22@gmail.com>
2025-04-30 09:23:45 -0300

    foi

---
+++ exercicio 1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int contador = 1;
+
+        while (contador <= 10)
+        {
+            Console.WriteLine("Pop.");
+            contador++;
+        }
+    }
+}
---
+++ exercicio 4/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int numero = 2;
+
+        while (numero <= N)
+        {
+            Console.WriteLine(numero);
+            numero += 2;
+        }
+    }
+}
